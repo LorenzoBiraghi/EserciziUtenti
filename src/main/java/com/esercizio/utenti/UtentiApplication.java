40ee41592dbf21f71459ccd9687ebf722586b6de
LorenzoBiraghi <lorenzo.biraghi@vidiemme.it>
2022-04-28 17:43:43 +0200

    CheckStyle main

--- src/main/java/com/esercizio/utenti/UtentiApplication.java
+++ src/main/java/com/esercizio/utenti/UtentiApplication.java
@@ -7,12 +7,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Users API", version = "1.0", description = "Users Information"))
-public class UtentiApplication {
-
-	/**
-	 * Main Run Application
-	 * @param args
-	 */
-	public static void main(String[] args){SpringApplication.run(UtentiApplication.class, args);}
-
-}
+public class UtentiApplication {public static void main(String[] args) {SpringApplication.run(UtentiApplication.class, args);}}
